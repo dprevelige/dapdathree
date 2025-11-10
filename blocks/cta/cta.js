705959fb7de4c5fb31ea1ec3d5a4c687e749956a
dprevelige <dpreveli@adobe.com>
2025-11-10 11:15:42 -0500

    rework js

--- blocks/cta/cta.js
+++ blocks/cta/cta.js
@@ -5,30 +5,26 @@ import {
   import { readBlockConfig } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-    console.log("cta", block);
+    console.log("block", block);
 
     const properties = readBlockConfig(block);
-    const rteElementTag = Array.from(block.querySelectorAll('p'))
-    .find((el) => el.textContent.trim() === 'buttontext');
-    const rteElement = rteElementTag?.parentElement?.nextElementSibling;
-    const rteContent = rteElement?.querySelector('p')?.innerHTML;
-    console.log("rteContent", rteContent);
     console.log("properties", properties);
     const cta = div({ class: 'cta-container' },
         div({ class: 'cta-wrapper' },
           div({ class: 'cta-image' },
             img({ src: properties.image, alt: properties.alt }),
           ),
-          div({ class: 'cta-text' }, (properties['keyhere']) ? properties['keyhere'] : ''),
+          div({ class: 'cta-text' }, (properties['ctaText']) ? properties['ctaText'] : ''),
           div({ class: 'cta-button' },
             div({ class: 'button-container' },
                 a({ id: 'button', href: '#', class: `button dark-bg` },
-                  span({ class: 'button-text' }, rteContent),
+                  span({ class: 'button-text' }, (properties['buttonText']) ? properties['buttonText'] : ''),
                 ),
               ),
           ),
         ),
       );
+      console.log("cta", cta);
       block.innerHTML = '';
       block.appendChild(cta);
 }
