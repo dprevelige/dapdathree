1b11929885abc1824fa6a965a318f8ffce6d5e85
dprevelige <dpreveli@adobe.com>
2025-11-10 10:58:27 -0500

    decorating cta

--- blocks/cta/cta.js
+++ blocks/cta/cta.js
@@ -9,10 +9,26 @@ export default function decorate(block) {
 
     const properties = readBlockConfig(block);
     const rteElementTag = Array.from(block.querySelectorAll('p'))
-    .find((el) => el.textContent.trim() === 'keyhere');
+    .find((el) => el.textContent.trim() === 'buttontext');
     const rteElement = rteElementTag?.parentElement?.nextElementSibling;
     const rteContent = rteElement?.querySelector('p')?.innerHTML;
     console.log("rteContent", rteContent);
     console.log("properties", properties);
-
+    const cta = div({ class: 'cta-container' },
+        div({ class: 'cta-wrapper' },
+          div({ class: 'cta-image' },
+            img({ src: properties.image, alt: properties.alt }),
+          ),
+          div({ class: 'cta-text' }, (properties['keyhere']) ? properties['keyhere'] : ''),
+          div({ class: 'cta-button' },
+            div({ class: 'button-container' },
+                a({ id: 'button', href: buttonLink, class: `button ${buttonStyle}` },
+                  span({ class: 'button-text' }, rteContent),
+                ),
+              ),
+          ),
+        ),
+      );
+      block.innerHTML = '';
+      block.appendChild(cta);
 }
